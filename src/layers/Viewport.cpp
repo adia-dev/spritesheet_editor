fc14a57bdaabfac671c4a57c015acbe2dd885101
Abdoulaye DIA <abdoudu78130@gmail.com>
2023-01-12 13:41:14 +0100

    move renderoverlay to cpp file

--- src/layers/Viewport.cpp
+++ src/layers/Viewport.cpp
@@ -147,7 +147,7 @@ namespace sse {
 
 		_renderTexture.setView(_renderTexture.getDefaultView());
 
-		RenderGrid(_renderTexture, _cellSize);
+		RenderGrid(_renderTexture, _cellSize, sf::Color(255, 255, 255, 50));
 		RenderSelection();
 
 		ImGui::Image(_renderTexture);
@@ -197,4 +197,39 @@ namespace sse {
 			_renderTexture.draw(rect);
 		}
 	}
+
+	template<typename Callback>
+	void Viewport::RenderOverlay(Callback&& callback) {
+		static int       location     = 0;
+		static bool      visible      = true;
+		ImGuiWindowFlags window_flags = ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_NoDocking |
+		                                ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoSavedSettings |
+		                                ImGuiWindowFlags_NoFocusOnAppearing | ImGuiWindowFlags_NoNav;
+
+		if (location >= 0) {
+			const float          PAD       = 10.0f;
+			const ImGuiViewport* viewport  = ImGui::GetMainViewport();
+			ImVec2               work_pos  = viewport->WorkPos; // Use work area to avoid menu-bar/task-bar, if any!
+			ImVec2               work_size = viewport->WorkSize;
+			ImVec2               window_pos, window_pos_pivot;
+			window_pos.x = (location & 1) ? (work_pos.x + work_size.x - PAD) : (work_pos.x + PAD);
+			window_pos.y =
+			    (location & 2) ? (work_pos.y + work_size.y - PAD) : (work_pos.y + PAD + ImGui::GetFrameHeight());
+			window_pos_pivot.x = (location & 1) ? 1.0f : 0.0f;
+			window_pos_pivot.y = (location & 2) ? 1.0f : 0.0f;
+			ImGui::SetNextWindowPos(window_pos, ImGuiCond_Always, window_pos_pivot);
+			ImGui::SetNextWindowViewport(viewport->ID);
+			window_flags |= ImGuiWindowFlags_NoMove;
+		} else if (location == -2) {
+			// Center window
+			ImGui::SetNextWindowPos(ImGui::GetMainViewport()->GetCenter(), ImGuiCond_Always, ImVec2(0.5f, 0.5f));
+			window_flags |= ImGuiWindowFlags_NoMove;
+		}
+		ImGui::SetNextWindowBgAlpha(0.75f); // Transparent background
+		if (ImGui::Begin("Overlay", &visible, window_flags)) {
+			callback(visible, location);
+		}
+		ImGui::End();
+	}
+
 } // namespace sse
--- src/layers/Viewport.h
+++ src/layers/Viewport.h
@@ -30,6 +30,7 @@ namespace sse {
 
 		// View
 		sf::View     _view;
+		sf::View     _minimapView;
 		sf::Vector2f _viewMousePos;
 		float        _viewSpeed           = 0.5f;
 		float        _zoom                = 1.f;
@@ -56,42 +57,10 @@ namespace sse {
 			return sf::Vector2<T>(lerp(left.x, right.x, x), lerp(left.y, right.y, x));
 		}
 
-		void
-		RenderGrid(sf::RenderTarget& target, float cellSize = 25.f, sf::Color color = sf::Color(111, 111, 111, 111));
+		void RenderGrid(sf::RenderTarget& target, float cellSize = 25.f, sf::Color color = sf::Color::White);
+		void RenderSelection();
 
 		template<typename Callback>
-		inline static void RenderOverlay(Callback&& callback) {
-			static int       location     = 0;
-			static bool      visible      = true;
-			ImGuiWindowFlags window_flags = ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_NoDocking |
-			                                ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoSavedSettings |
-			                                ImGuiWindowFlags_NoFocusOnAppearing | ImGuiWindowFlags_NoNav;
-
-			if (location >= 0) {
-				const float          PAD       = 10.0f;
-				const ImGuiViewport* viewport  = ImGui::GetMainViewport();
-				ImVec2               work_pos  = viewport->WorkPos; // Use work area to avoid menu-bar/task-bar, if any!
-				ImVec2               work_size = viewport->WorkSize;
-				ImVec2               window_pos, window_pos_pivot;
-				window_pos.x = (location & 1) ? (work_pos.x + work_size.x - PAD) : (work_pos.x + PAD);
-				window_pos.y =
-				    (location & 2) ? (work_pos.y + work_size.y - PAD) : (work_pos.y + PAD + ImGui::GetFrameHeight());
-				window_pos_pivot.x = (location & 1) ? 1.0f : 0.0f;
-				window_pos_pivot.y = (location & 2) ? 1.0f : 0.0f;
-				ImGui::SetNextWindowPos(window_pos, ImGuiCond_Always, window_pos_pivot);
-				ImGui::SetNextWindowViewport(viewport->ID);
-				window_flags |= ImGuiWindowFlags_NoMove;
-			} else if (location == -2) {
-				// Center window
-				ImGui::SetNextWindowPos(ImGui::GetMainViewport()->GetCenter(), ImGuiCond_Always, ImVec2(0.5f, 0.5f));
-				window_flags |= ImGuiWindowFlags_NoMove;
-			}
-			ImGui::SetNextWindowBgAlpha(0.75f); // Transparent background
-			if (ImGui::Begin("Overlay", &visible, window_flags)) {
-				callback(visible, location);
-			}
-			ImGui::End();
-		}
-		void RenderSelection();
+		static void RenderOverlay(Callback&& callback);
 	};
 } // namespace sse
